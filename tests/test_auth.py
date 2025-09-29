800984c00d9bf835591adf5e49f54c3930a09489
Alexkamilov5 <alexkamilov5@gmail.com>
2025-09-29 18:31:31 +0500

    Update test_auth.py

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -7,12 +7,12 @@ client = TestClient(app)
 
 def test_register_and_login():
     # регистрация — JSON
-    r = client.post("/auth/register", json={"username": "bob8", "password": "secret"})
+    r = client.post("/auth/register", json={"username": "bob9", "password": "secret"})
     assert r.status_code == 200
 
     # логин — ФОРМА (а не json!)
     r2 = client.post(
-        "/auth/login", data={"username": "bob8", "password": "secret"}  # <-- важно
+        "/auth/login", data={"username": "bob9", "password": "secret"}  # <-- важно
     )
     assert r2.status_code == 200
     data2 = r2.json()
