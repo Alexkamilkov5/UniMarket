9cee2af8a634485f82084648df8dc6dd00cba334
Alexkamilkov5 <alexkamilov5@gmail.com>
2025-11-04 15:37:11 +0500

    models.py+alembic fix+migration povtorno dlya github.com

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from typing import List, Optional
+
 from sqlalchemy import ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
@@ -19,23 +21,27 @@ class User(Base):
 class Category(Base):
     __tablename__ = "categories"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name: Mapped[int] = mapped_column(String, unique=True, index=True)
+    name: Mapped[str] = mapped_column(String, unique=True, index=True)
     # обратная связь — доступ ко всем товарам категории
-    items = relationship("Item", back_populates="category")
+    # items = relationship("Item", back_populates="category")
+    items: Mapped[List["Item"]] = relationship("Item", back_populates="category")
 
 
 class Item(Base):
     __tablename__ = "items"
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
-    name = mapped_column(String, index=True)
-    description: Mapped[int] = mapped_column(String, nullable=True)
+    name: Mapped[str] = mapped_column(String, index=True)
+    description: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     price: Mapped[int] = mapped_column(Integer)
-    category_id: Mapped[int] = mapped_column(  # ВАЖНО
-        ForeignKey("categories.id"),
+    category_id: Mapped[Optional[int]] = mapped_column(
+        ForeignKey("categories.id", name="fk_items_category_id"),
         nullable=True,
         index=True,
     )
 
     # category_id: Mapped[int] = mapped_column(Integer, ForeignKey("categories.id"))
     # связь с категорией
-    category = relationship("Category", back_populates="items")
+    # category = relationship("Category", back_populates="items")
+    category: Mapped[Optional["Category"]] = relationship(
+        "Category", back_populates="items"
+    )
